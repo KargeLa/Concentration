c35d0b2f313baf7021722954092b0bdebcdb942a
KargeLa <38924843+KargeLa@users.noreply.github.com>
2020-01-14 16:49:53 +0300

    Add files via upload

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Алексей Смоляк on 1/10/20.
+//  Copyright © 2020 Алексей Смоляк. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    //MARK: - Actions
+    @IBAction func cardButtonClicked(_ sender: Any) {
+    }
+    
+    //MARK: - LifeCircle
+    func flipCard(withEmoji emoji: Srting, on Button: UIButton)
+    
+    override func viewDidLoad() {
+    }
+    
+    
+}
+
